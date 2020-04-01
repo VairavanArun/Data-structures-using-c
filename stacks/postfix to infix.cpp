5dc86fbf8de0b0cc77ada1b59a02a4ded4d2d7c4
Vairavan <45746396+VairavanArun@users.noreply.github.com>
2020-04-01 20:40:40 +0530

    Postfix to infix along with algorithm

--- stacks/postfix to infix.cpp
+++ stacks/postfix to infix.cpp
@@ -1,4 +1,28 @@
 /*postfix to infix expressions*/
+/*algorithm for postfix to infix conversion*/
+/*
+step 1: read the post fix expression
+step 2: if the charachter is an operand push into stack
+step 3: if the charachter is an operator, pop two operands out of the stack, insert the operator in between the popped operands
+and push the resultant expression onto the stack
+step 4: repeat the process until the enter expression is read
+At the end the top of the stack will be the resultant infix expression
+
+Note:
+1.Stack have been implemented using arrays.
+2.IMPORTANT: The data which we are pushing into the stack is either an operand or an expression. 
+Operand is of lenght one charachter, but expression is of variable length. The expression may be pushed into any location of the 
+array. This indicates that each element of array should be of variable length i.e 2d dynamically allocated array is needed
+That is why I have declared the array element of stack as a pointer to pointer. I have followed pointer to pointer method of dynamic 
+allocation of 2D array. 
+3.I have used two push functions, one for operand push another for expression push. The operand push pushes only one charachter onto the
+stack, pushExpression pushes an expression to stack. Why there are two separate functions for operands and expressions. I have used 
+strcpy() fucntion to copy the entire string on to the stack. strcpy() requires two char* and copies the source string upto first null to
+destination string. If we use the same function for both operand and expression, the operand is passed as a single charachter and not as
+charachter pointer, if we pass it as charachter pointer then strcpy() results in copying the exp string from the encountered operand to
+the first null, that is the rest of the expression, which is undesired. That is why I have used two different functions for pushing 
+operand and pushing expression
+*/
 
 #include<stdio.h>
 #include<stdlib.h>
